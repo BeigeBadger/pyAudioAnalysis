b7685ccafac173107538156a56196beffd438894
Matt Stannett (AWARE) <matt.stannett@awaregroup.com>
2019-10-30 16:02:51 +1300

    Adds instructions and other resources.

---
+++ BatchChunkMonoByDuration.ps1
@@ -0,0 +1,13 @@
+﻿# TODO - Update this path to match where your files are stored
+$filesToProcess = Get-ChildItem -Path C:\Users\mastan\Downloads\Work\Tools\ffmpeg-20190909-976617c-win64-static\bin\Audio\*.wav -Name
+
+foreach ($monoFile in $filesToProcess) {
+    $oldFileNameWithoutExtension = $monoFile.substring(0, $monoFile.Length - 9)
+    $monoFile = "$($oldFileNameWithoutExtension)-mono.wav"
+    $outputDirectory = "$oldFileNameWithoutExtension-parts"
+
+    mkdir $outputDirectory
+
+    # Change 30 to any duration you'd like (in seconds)
+    ffmpeg -i $monoFile -f segment -segment_time 30 -c copy "$outputDirectory/$oldFileNameWithoutExtension-part%03d.wav"
+}
---
+++ BatchConvertStereoToMono.ps1
@@ -0,0 +1,9 @@
+﻿# TODO - Update this path to match where your files are stored
+$filesToProcess = Get-ChildItem -Path C:\Users\mastan\Downloads\Work\Tools\ffmpeg-20190909-976617c-win64-static\bin\Audio\*.wav -Name
+
+foreach ($stereoFile in $filesToProcess) {
+    $oldFileNameWithoutExtension = $stereoFile.substring(0, $stereoFile.Length - 4)
+    $monoFile = "$($oldFileNameWithoutExtension)-mono.wav"
+
+    ffmpeg -i $stereoFile -ac 1 $monoFile
+}
