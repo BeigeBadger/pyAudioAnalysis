57a0d811fbf14c9cbb6641272f8ce0e2f542fe11
Matt Stannett (AWARE) <matt.stannett@awaregroup.com>
2019-10-30 15:51:55 +1300

    Adds TODOs.

--- pyAudioAnalysis/split_continuous_audio.py
+++ pyAudioAnalysis/split_continuous_audio.py
@@ -7,7 +7,9 @@ for file in sorted(glob.glob("*.wav")):
 
     os.mkdir(directory)
 
-    cmd_to_run = "python C:\\Users\\mastan\\Documents\\repos\\personal\\pyAudioAnalysis\\pyAudioAnalysis\\audioAnalysis.py silenceRemoval --input " + file + " --output " + directory + " --smoothing 1.0 --weight 0.3"
+    # TODO: Update this path to match where your code lives
+    # TODO: Update the minDuration and maxDuration args to match your requirements
+    cmd_to_run = "python C:\\Users\\mastan\\Documents\\repos\\personal\\pyAudioAnalysis\\pyAudioAnalysis\\audioAnalysis.py silenceRemoval --input " + file + " --output " + directory + " --minDuration 5.0 --maxDuration 10.0 --smoothing 1.0 --weight 0.3"
 
     print("#"*40)
     print(file)
